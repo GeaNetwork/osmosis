cbbff1d15e6afc50c39894c4adbff1299c462d7b
aris <ningxin1718@gmail.com>
2024-12-25 19:34:41 +0800

    fix: set account prefix as gea

--- app/params/config.go
+++ app/params/config.go
@@ -8,16 +8,16 @@ import (
 
 const (
 	// DisplayDenom is the denomination used to display the amount of tokens held
-	DisplayDenom = "dym"
+	DisplayDenom = "gea"
 	// BaseDenomUnit is the base denom unit for the Hub
-	BaseDenom = "adym"
+	BaseDenom = "ugea"
 
 	// BaseDenomUnit defines the base denomination unit for Photons.
 	// 1 DYM = 1x10^{BaseDenomUnit} adym
 	BaseDenomUnit = 18
 
-	AccountAddressPrefix = "dym"
-	Name                 = "dymension"
+	AccountAddressPrefix = "gea"
+	Name                 = "gea"
 )
 
 func init() {
