c70836840a94d22e564116c641d96b66b153ed61
lsq984 <lsq984@163.com>
2025-08-06 10:25:07 +0800

    change address prefix from ec to gc

--- app/params/config.go
+++ app/params/config.go
@@ -8,16 +8,16 @@ import (
 
 const (
 	// DisplayDenom is the denomination used to display the amount of tokens held
-	DisplayDenom = "ec"
+	DisplayDenom = "gc"
 	// BaseDenomUnit is the base denom unit for the Hub
-	BaseDenom = "uec"
+	BaseDenom = "ugc"
 
 	// BaseDenomUnit defines the base denomination unit for Photons.
 	// 1 DYM = 1x10^{BaseDenomUnit} adym
 	BaseDenomUnit = 8
 
-	AccountAddressPrefix = "ec"
-	Name                 = "ec"
+	AccountAddressPrefix = "gc"
+	Name                 = "gc"
 )
 
 func init() {
